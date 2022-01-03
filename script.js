dc57c59cce2233ce053d4fcac14721c4edad9284
Austin Torres <78386688+austintorres578@users.noreply.github.com>
2022-01-03 14:03:39 -0500

    Weather categories added
    Added humidity, wind, and UV index to app and also changed the layout of the menu

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const temperture=document.getElementById("temperture-text");
 const locationText=document.getElementById("location-text");
+const humidityText=document.getElementById("humidity-subtitle");
+const windText=document.getElementById("wind-subtitle");
+const ultravioletText=document.getElementById("ultraviolet-subtitle");
 const feel=document.getElementById("feels-like-text");
 const zipInput=document.getElementById("zip-input");
 const submitButton=document.getElementById("submit-button");
+const weatherImagesContainer=document.getElementById("weather-images-container");
+const weatherCatagoriesContainer=document.getElementById("weather-catagories-container");
+const weatherInputContainer=document.getElementById("weather-input-container");
+
+let dataTemperture;
+let dataLocationCity;
+let dataLocationState;
+let dataFeel;
+let dataHumidity;
+let dataWind;
+let dataUltraviolet;
 
 submitButton.addEventListener("click",test);
 
@@ -10,19 +24,22 @@ function test(){
 fetch("http://api.weatherapi.com/v1/current.json?key=b28574dd6599479e944222901212812&q="+zipInput.value+"&aqi=no")
 .then(Response => Response.json())
 .then(data => {
-    let dataTemperture;
-    let dataLocationCity;
-    let dataLocationState;
-    let dataFeel;
     zipInput.value="";
     dataFeel=data.current.feelslike_f+"℉";
     dataLocationCity=data.location.name;
     dataLocationState=data.location.region;
     dataTemperture=data.current.temp_f+"℉";
+    weatherImagesContainer.style.display="block";
+    weatherCatagoriesContainer.style.display="flex";
+    weatherInputContainer.style.display="none";
 
     temperture.innerText=dataTemperture;
     locationText.innerText=dataLocationCity+", "+dataLocationState;
+    humidityText.innerText=data.current.humidity+"%";
+    windText.innerText=data.current.wind_mph+"mph";
+    ultravioletText.innerText=data.current.uv+"/10"
     feel.innerText="Feels like "+dataFeel;
+    console.log(data);
 })
 .catch(error=>alert("Incorrect Zipcode"))
 };
